d9fcdec9cd73834795aa9f857e9fe86248d6e073
Baijie Chen <vchen8@terpmail.umd.edu>
2022-11-29 18:39:20 -0500

    encrypt and decrypt are now sepecific for aes256 cbc mode, for other mode,...

--- bank/bank-main.c
+++ bank/bank-main.c
@@ -11,39 +11,48 @@
 #include "bank.h"
 #include "ports.h"
 
+
 static const char prompt[] = "BANK: ";
 
-int main(int argc, char**argv)
-{
+int main(int argc, char**argv) {
    int n;
-   char sendline[1000];
-   char recvline[1000];
+   char sendline[DATASIZE + 1] = "";
+   unsigned char recvline[DATASIZE + 1] = "";
+   FILE *input;
+   unsigned char key[KEY_LEN + 1]  = "";
 
-   Bank *bank = bank_create();
+   if ((input = fopen(argv[1], "r")) == NULL) {
+      printf("Error opening bank initialization file\n");
+      return 64;
+   } 
+   fread(key, 1, KEY_LEN, input);
+   fclose(input);
+
+   Bank *bank = bank_create(key);
 
    printf("%s", prompt);
    fflush(stdout);
 
-   while(1)
-   {
-       fd_set fds;
-       FD_ZERO(&fds);
-       FD_SET(0, &fds);
-       FD_SET(bank->sockfd, &fds);
-       select(bank->sockfd+1, &fds, NULL, NULL, NULL);
+   while(1) {
+      fd_set fds;
+      FD_ZERO(&fds);
+      FD_SET(0, &fds);
+      FD_SET(bank->sockfd, &fds);
+      select(bank->sockfd+1, &fds, NULL, NULL, NULL);
 
-       if(FD_ISSET(0, &fds))
-       {
-           fgets(sendline, 10000,stdin);
-           bank_process_local_command(bank, sendline, strlen(sendline));
-           printf("%s", prompt);
-           fflush(stdout);
-       }
-       else if(FD_ISSET(bank->sockfd, &fds))
-       {
-           n = bank_recv(bank, recvline, 10000);
-           bank_process_remote_command(bank, recvline, n);
-       }
+      if(FD_ISSET(0, &fds)) {
+         fgets(sendline, DATASIZE + 1, stdin);
+         
+         if (sendline[strlen(sendline) - 1] != '\n') {
+            while (getchar() != '\n'); //clear rest of stdin
+         }
+         bank_process_local_command(bank, sendline, strlen(sendline));
+         printf("%s", prompt);
+         fflush(stdout);
+      } else if(FD_ISSET(bank->sockfd, &fds)) {
+         n = bank_recv(bank, recvline, DATASIZE);
+         bank_process_remote_command(bank, recvline, n);
+      }
    }
 
    return EXIT_SUCCESS;
